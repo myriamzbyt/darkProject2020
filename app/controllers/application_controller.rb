7d949cb5fe6934f61e144b324bb50625581d7832
fagaudin <farid-yve-andre.gaudin@capgemini.com>
2020-12-07 18:44:46 +0100

    début de l'application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  def index
+    params[:id] =1
+  end
+
+  def principal
+    @Member = 'A saisir'
+  end
+
+  def resume
+    @info_famille = nil
+  end
+
+
+  def create_family
+  end
 end
